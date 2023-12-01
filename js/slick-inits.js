293cb0d5f4d7aef4971f09ca2cdf4ab0309c54c2
Andrew <nolis920@gmail.com>
2023-12-01 19:38:37 +0300

    init

---
+++ js/slick-inits.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $('.studio-slider').slick({
+    prevArrow: "<img class='slick__arrow--left' src='../icons/arrow-left.svg' />",
+    nextArrow: "<img class='slick__arrow--right' src='../icons/arrow-right.svg' />"
+  });
+  $('.slider-four__slider').slick({
+    prevArrow: "<img class='slick__arrow--left' src='../icons/arrow-left.svg' />",
+    nextArrow: "<img class='slick__arrow--right' src='../icons/arrow-right.svg' />"
+  });
+  $('.interview-slider').slick({
+    prevArrow: "<img class='slick__arrow--left' src='../icons/arrow-left.svg' />",
+    nextArrow: "<img class='slick__arrow--right' src='../icons/arrow-right.svg' />"
+  })
+});
