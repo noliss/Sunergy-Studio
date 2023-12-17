24d1c4a8326c343a16f4e772fa8358d82caf57b7
Andrew <nolis920@gmail.com>
2023-12-17 18:17:03 +0300

    upd

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,30 @@
-$('.faq__block').on('click', '.faq__item', function() {
-  var answer = $(this).find('.faq__answer');
-  $(this).siblings().find('.faq__answer').slideUp();
+$(".faq__block").on("click", ".faq__item", function () {
+  var answer = $(this).find(".faq__answer");
+  $(".faq__answer").not(answer).slideUp();
   answer.slideToggle();
-  $(this).toggleClass('faq__item--active');
-  $(this).siblings().removeClass('faq__item--active');
+  $(".faq__item").not(this).removeClass("faq__item--active");
+  $(this).toggleClass("faq__item--active");
 });
 
 // Бургер
-$('.header-menu__mobile-burger').click(function() {
-  console.log('123');
-  $('.header-menu__mobile-burger').toggleClass('open-menu');
-  $('.header-menu__mobile-nav').toggleClass('header-menu__mobile-nav--hidden');
+$(".header-menu__mobile-burger").click(function () {
+  console.log("123");
+  $(".header-menu__mobile-burger").toggleClass("open-menu");
+  $(".header-menu__mobile-nav").toggleClass("header-menu__mobile-nav--hidden");
 });
 
-// 
+//
 Fancybox.bind("[data-fancybox]", {
   // Your custom options
+});
+
+$(
+  ".about-info__button, .partners__button, .application__button, .sert__button, .sert-second__button"
+).click(function () {
+  $("html, body").animate(
+    {
+      scrollTop: $(".form").offset().top,
+    },
+    800
+  );
 });
